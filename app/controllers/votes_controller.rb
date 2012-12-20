3d253a1e6c416c520f697322687e55e37ec13cc9
J. Michael Kasiewicz <crestcode@gmail.com>
2012-12-20 13:32:51 -0500

    Add AJAX to voting controls on index and player pages.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,13 @@ class VotesController < ApplicationController
 	def create
 		post = Post.find(params[:post_id])
 		post.votes.create(params[:vote])
-		redirect_to root_path
+	 	respond_to do |format|
+	 		format.html { redirect_to root_path }
+	 		format.js do
+	 			@post = post.id.to_json
+	 			@post_votes = post.vote_number.to_json
+	 		end 
+	 end
 	end
 
 end
