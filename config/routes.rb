d5be286877b7f2df54f06adf93bc4dfd763ce07c
John Kasiewicz <crestcode@gmail.com>
2014-07-19 15:22:00 -0400

    add route for tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CourseProject::Application.routes.draw do
   root to: "posts#index"
 
+  get 'tags/:tag', to: 'posts#index', as: :tag
+
   resources :posts, :only => [:index, :new, :create, :show] do
   	resources :votes, only: [:create]
   end
