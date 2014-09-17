5e9fe06a0a7c6c84fd61ba9496f7a9225f698aea
John Kasiewicz <crestcode@gmail.com>
2014-09-17 14:46:53 -0400

    disable search in production

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,9 +16,11 @@ class Post < ActiveRecord::Base
   scope :by_created_at, ->(page) { order(created_at: :desc).page(page).per(5) }
   scope :by_tag, ->(tag, page) { tagged_with(tag).by_created_at(page) }
 
-  searchable do
-    text :artist
-    text :title
+  unless Rails.env.production?
+    searchable do
+        text :artist
+        text :title
+    end
   end
 
   def url_contains_playable_content
