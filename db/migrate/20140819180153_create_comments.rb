1ab97a334ce09af0dfdbfbc0ecdb7e284a6342e6
John Kasiewicz <crestcode@gmail.com>
2014-10-27 15:34:58 -0400

    fix create comments migration

--- db/migrate/20140819180153_create_comments.rb
+++ db/migrate/20140819180153_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    create_table :reviews do |t|
+    create_table :comments do |t|
       t.text :body
       t.references :post, index: true
       t.references :user, index: true
